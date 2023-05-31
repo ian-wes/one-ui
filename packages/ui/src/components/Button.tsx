cce5138ae58e241bde8b32260edebd6787e61250
Ian Ah Po <ipo@wesdigital.com.au>
2023-05-31 17:09:07 +1000

    feat: Styling button

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -1,17 +1,16 @@
-import {Button as BaseButton, ButtonProps as BaseButtonProps, Spinner} from 'tamagui'
+import {Button as BaseButton, ButtonProps as BaseButtonProps, styled} from 'tamagui'
 
 export type ButtonProps = BaseButtonProps & {
     loading?: boolean
 }
 
-export const Button = (props: ButtonProps) => {
-    return (
-        <BaseButton
-            {...props}
-            {...(props.loading && {
-                icon: <Spinner />,
-                disabled: true,
-            })}
-        />
-    )
-}
+export const Button = styled(BaseButton, {
+    borderRadius: '$12',
+    backgroundColor: "#00E28F",
+    color: '#3a006a',
+    width: '100%',
+    maxWidth: 400,
+    fontWeight: '700',
+    fontSize: '16px',
+    height: 56,
+})
